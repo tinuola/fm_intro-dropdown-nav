f58a6a24c0f208ca460c8725ad96fe2b3f9dd01a
tinuola <24995224+tinuola@users.noreply.github.com>
2024-04-17 00:48:43 -0700

    feat: add javascript for nav menu

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-console.log('hello')
+const dropdown = document.querySelectorAll('.dropdown')
+
+const setImage = () => {}
+
+dropdown.forEach((menu) => {
+  menu.addEventListener('click', function (e) {
+    // console.log(e)
+    // console.log(menu.style)
+    menu.lastElementChild.classList.toggle('hide-menu')
+    menu.lastElementChild.classList.toggle('show-menu')
+
+    menu.firstElementChild.firstElementChild.setAttribute(
+      'src',
+      '../assets/images/icon-arrow-up.svg'
+    )
+  })
+})
