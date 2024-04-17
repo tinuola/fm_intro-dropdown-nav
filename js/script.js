7d37324fe557ff228b53c7cbd30b6330db4d71da
tinuola <24995224+tinuola@users.noreply.github.com>
2024-04-17 01:41:12 -0700

    feat: implement menu toggle

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
 const dropdown = document.querySelectorAll('.dropdown')
 
-const setImage = () => {}
-
 dropdown.forEach((menu) => {
   menu.addEventListener('click', function (e) {
     // console.log(e)
     // console.log(menu.style)
+
+    const dropdownIcon = menu.querySelectorAll('.dropdown-icon')
+
+    dropdownIcon.forEach((icon) => {
+      icon.classList.toggle('hide-menu')
+    })
+
     menu.lastElementChild.classList.toggle('hide-menu')
     menu.lastElementChild.classList.toggle('show-menu')
-
-    menu.firstElementChild.firstElementChild.setAttribute(
-      'src',
-      '../assets/images/icon-arrow-up.svg'
-    )
   })
 })
